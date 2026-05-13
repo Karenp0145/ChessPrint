f1bb0b3573c9606a61555363258411452dacd002
Chipsoupe <belletpaul999@gmail.com>
2026-05-13 15:44:25 +0200

    update webhook URL in CtaSection component

--- src/components/CtaSection.vue
+++ src/components/CtaSection.vue
@@ -8,7 +8,7 @@ const { show } = useToast()
 
 const email = ref('')
 const isSubmitting = ref(false)
-const WEBHOOK_URL = 'https://hook.eu1.make.com/0hj8f8em4i9qd7scyqktw5jwc0hljyat'
+const WEBHOOK_URL = 'https://hook.eu1.make.com/sdixoolegn3dall15tq3e4i9a376bif4'
 
 async function submit() {
   if (!email.value.trim()) {
